dfb7a3cc59400e6cbaeb76f62cdee18398a977ac
Bae,eun kyoung <eun97052425@gmail.com>
2018-07-08 22:22:03 +0900

    공지사항 리스트 조회

--- src/main/java/com/kh/hamo/dao/ClubBbsInter.java
+++ src/main/java/com/kh/hamo/dao/ClubBbsInter.java
@@ -1,5 +1,12 @@
 package com.kh.hamo.dao;
 
+import java.util.ArrayList;
+
+import com.kh.hamo.dto.ClubBbsDTO;
+
 public interface ClubBbsInter {
+	
+//공지사항 리스트
+ArrayList<ClubBbsDTO> clubNoticeList(int club_id, String sort, String member_id);
 
 }
--- src/main/java/com/kh/hamo/service/ClubBbsService.java
+++ src/main/java/com/kh/hamo/service/ClubBbsService.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.hamo.dao.ClubBbsInter;
 
@@ -16,6 +17,16 @@ public class ClubBbsService {
     @Autowired
 	private SqlSession sqlSession;
     
-    ClubBbsInter inter;
+    ClubBbsInter clubBbsInter = null;
+    
+	//공지사항 리스트
+	public ModelAndView clubNoticeList(String member_id, int club_id,String sort) {
+		clubBbsInter = sqlSession.getMapper(ClubBbsInter.class);
+		ModelAndView mav = new ModelAndView();
+		logger.info("리스트 불러오기");
+		mav.addObject("list", clubBbsInter.clubNoticeList(club_id, sort,member_id));
+		mav.setViewName("c05");
+		return mav;
+	}
 
 }
