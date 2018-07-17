93fcbe99ea97832730bacf0db8a1bcbad5b815e3
lee min-choel <lmc0129@naver.com>
2018-07-17 10:45:06 +0900

    모임일정 리스트 수정 , 모임일정 참석자 수정

--- src/main/java/com/kh/hamo/dao/ClubMeetingPlanInter.java
+++ src/main/java/com/kh/hamo/dao/ClubMeetingPlanInter.java
@@ -6,25 +6,27 @@ import com.kh.hamo.dto.ClubMeetingDTO;
 
 public interface ClubMeetingPlanInter {
 
-	ArrayList<ClubMeetingDTO> clubMeetingList(int club_id);//모임 일정 리스트
+	ArrayList<ClubMeetingDTO> clubMeetingList(int club_id, String member_id);//모임 일정 리스트
 
 
 	int clubMeetingWrite(Object club_id, Object member_id, Object meetingPlan_subject,
 			Object meetingPlan_locationX, Object meetingPlan_locationY, Object meetingPlan_when,
 			Object meetingPlan_money, Object meetingPlan_content);//모임 일정 등록
 
-
-	int clubMeetingCount(String club_id);
-
-
 	void clubMeetingWriteFirst(int parseInt, String member_id, String meetingPlan_subject, String meetingPlan_locationX,
 			String meetingPlan_locationY, String meetingPlan_when, String meetingPlan_money,
 			String meetingPlan_content);
+	
+	
+	int clubMeetingCount(String club_id);
+
 
 
 	ClubMeetingDTO clubMeetingDetail(int parseInt);//모임 일정 상세보기 
 
 
-	ArrayList<String> meetingAttend(int parseInt);//모임 일정 참석자
+	ArrayList<String> meetingAttend(int meetingPlan_id, int club_id);//모임 일정 참석자
+
+
 
 }
